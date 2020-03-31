956eea828478db5f4ca445880f9b745bc0b3910c
vokadub <o.dubakov@gmail.com>
2020-03-31 20:27:09 +0300

    * header was added

---
+++ TrelloToDoList.Web/src/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  render() {
+      return (
+        <header>
+          <span>Trello ToDo List</span>
+        </header>);
+  }
+}
+
+export default Header;
--- TrelloToDoList.Web/src/Component/MainPage/MainPage.js
+++ TrelloToDoList.Web/src/Component/MainPage/MainPage.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './MainPage.css';
 import UserName from '../UserName/UserName';
 import TaskList from '../TaskList/TaskList';
+import Header from '../Header/Header';
 
 class MainPage extends React.Component {
   render() {
       return ( 
-        <div className="main-page">
-          <h1>
-            Trello ToDo List
-          </h1>
-          <UserName name="User Name"/>
-          <TaskList />
+        <div className="page">
+          <Header/>
+          <main className="content">
+            <UserName name="User Name"/>
+            <TaskList />
+          </main>
         </div>);
   }
 }

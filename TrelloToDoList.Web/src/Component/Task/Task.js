d55691682628b47edfae01e42c8a82fea739ec3e
vokadub <o.dubakov@gmail.com>
2020-04-02 23:10:41 +0300

    * task 'state change' was added

--- TrelloToDoList.Web/src/Component/Task/Task.js
+++ TrelloToDoList.Web/src/Component/Task/Task.js
@@ -4,10 +4,22 @@ import './Task.css';
 // TODO: change state of marked tasks
 
 class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { marked: false };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    var currentState = this.state.marked;
+    this.setState({ marked: !currentState});
+  }
+
   render() {
       return (
-        <div className="task">
-          <input type="checkbox" />
+        <div className={this.state.marked ? "task marked" : "task"}>
+          <input type="checkbox" onClick={this.toggle}/>
           <span className="task-name">
             {this.props.name}
           </span>

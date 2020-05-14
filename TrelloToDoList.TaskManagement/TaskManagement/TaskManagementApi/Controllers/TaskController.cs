c0e5217cbc9ec3a824ebd8f7296252f86c8e7435
vokadub <o.dubakov@gmail.com>
2020-05-14 23:10:41 +0300

    Initial commit of task management service

---
+++ TrelloToDoList.TaskManagement/TaskManagement/TaskManagementApi/Controllers/TaskController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementApi.Models;
+
+namespace TaskManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        // GET: api/Task
+        [HttpGet]
+        public IEnumerable<TaskItem> Get()
+        {
+            return Enumerable.Empty<TaskItem>();
+        }
+
+        // GET: api/Task/5
+        [HttpGet("{id}", Name = "Get")]
+        public TaskItem Get(string id)
+        {
+            return new TaskItem(
+                "id", 
+                "title", 
+                "description", 
+                "user id");
+        }
+
+        // POST: api/Task
+        [HttpPost]
+        public void Post([FromBody] TaskItem value)
+        {
+        }
+
+        // PUT: api/Task/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] TaskItem value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+        }
+    }
+}
---
+++ TrelloToDoList.TaskManagement/TaskManagement/TaskManagementApi/Models/TaskItem.cs
@@ -0,0 +1,47 @@
+﻿namespace TaskManagementApi.Models
+{
+    /// <summary>
+    /// Task.
+    /// </summary>
+    public class TaskItem
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TaskItem"/> class.
+        /// </summary>
+        /// <param name="id">Task id.</param>
+        /// <param name="title">Title.</param>
+        /// <param name="description">Description.</param>
+        /// <param name="userId">User id.</param>
+        public TaskItem(
+            string id, 
+            string title, 
+            string description, 
+            string userId)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            UserId = userId;
+        }
+
+        /// <summary>
+        /// Task id.
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        /// Title.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// Description.
+        /// </summary>
+        public string Description { get; }
+
+        /// <summary>
+        /// User id.
+        /// </summary>
+        public string UserId { get; }
+    }
+}
